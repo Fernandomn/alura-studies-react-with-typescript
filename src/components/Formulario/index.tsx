eaf87ca3f32ba66d1a8581eb461f037d7621d3b6
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-11-21 16:34:45 -0300

    changing styles to css modules

--- src/components/Formulario/index.tsx
+++ src/components/Formulario/index.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Botao from "../Botao";
-import "./style.scss";
+import style from "./Formulario.module.scss";
 
 class Formulario extends React.Component {
   render(): React.ReactNode {
     return (
-      <form className="novaTarefa">
-        <div className="inputContainer">
+      <form className={style.novaTarefa}>
+        <div className={style.inputContainer}>
           <label htmlFor="tarefa">Adicione um novo estudo</label>
+
           <input
             type="text"
             name="tarefa"
@@ -17,7 +18,7 @@ class Formulario extends React.Component {
           ></input>
         </div>
 
-        <div className="inputContainer">
+        <div className={style.inputContainer}>
           <label htmlFor="tempo">Tempo</label>
           <input
             type="time"
--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,4 +1,4 @@
-import "./style.scss";
+import style from "./Lista.module.scss";
 
 function Lista() {
   const tarefas = [
@@ -8,12 +8,14 @@ function Lista() {
   ];
 
   return (
-    <aside className="listaTarefas">
+    <aside className={style.listaTarefas}>
       <h2>Estudos do dia</h2>
+
       <ul>
         {tarefas.map((item, index) => (
-          <li className="item" key={index}>
+          <li className={style.item} key={index}>
             <h3>{item.tarefa}</h3>
+
             <span>{item.tempo}</span>
           </li>
         ))}
