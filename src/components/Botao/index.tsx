2039904d503df8c00f6e16fb14ea21118206cf02
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2023-11-21 15:31:44 -0300

    creating basic structure, and components Botao, Formulario and Lista

---
+++ src/components/Botao/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Botao extends React.Component {
+  render() {
+    const isActive = true;
+    const styles = { backgroundColor: isActive ? "green" : "red" };
+
+    return <button style={styles}>Botão</button>;
+  }
+}
+
+export default Botao;
